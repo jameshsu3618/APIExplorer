e9e06a6e21a21526d0d3263f97d6deb7b4378896
James <james@brandboom.com>
2019-08-02 11:21:37 -0700

    UPDATE SettingsModal.vue update formatting and spacing

--- src/components/SettingsModal.vue
+++ src/components/SettingsModal.vue
@@ -1,49 +1,54 @@
 <template>
 	<v-flex>
-	<div>
-		<div xs8 class="settings-menu">
-			<div class="header">
-			iTunes Explorer Settings
+		<div>
+			<div xs8 class="settings-menu">
+				<div class="header">
+					iTunes Explorer Settings
+				</div>
+				<div class="setting-row">
+					<div class="settings">
+						Num. of Results
+					</div>
+					<select class="settings setting-select" v-model="settings['numResults']">
+						<option disabled value="">Please select one</option>
+						<option>50</option>
+						<option>100</option>
+						<option>200</option>
+					</select>
+				</div>
+				<div class="setting-row">
+					<div class="settings">
+						Country
+					</div>
+					<select class="settings setting-select" v-model="settings['country']">
+						<option disabled value="">Please select one</option>
+						<option v-for="(country, index) in countries" :value="index">{{country}}</option>
+					</select>
+				</div>
+				<div class="setting-row">
+					<div class="settings">
+						Button Color
+					</div>
+					<select class='settings setting-select' v-model="settings['btnColor']">
+						<option disabled value="">Please select one</option>
+						<option v-for="(color, index) in btnColor" :value="color"><span :class="color">{{color}}</span></option>
+					</select>
+				</div>
 			</div>
-			<div class="setting-row"><div class="settings">
-				Num. of Results</div>
-				<select class="settings setting-select" v-model="settings['numResults']">
-				  <option disabled value="">Please select one</option>
-				  <option>50</option>
-				  <option>100</option>
-				  <option>200</option>
-				</select>
-			</div>
-			<div class="setting-row"><div class="settings">
-				Country</div>
-				<select class="settings setting-select" v-model="settings['country']">
-				  <option disabled value="">Please select one</option>
-				  <option v-for="(country, index) in countries" :value="index">{{country}}</option>
-				</select>
-			</div>
-			<div class="setting-row"><div class="settings">
-				Button Color</div>
-				<select class='settings setting-select' v-model="settings['btnColor']">
-				  <option disabled value="">Please select one</option>
-				  <option v-for="(color, index) in btnColor" :value="color"><span :class="color">{{color}}</span></option>
-				</select>
-			</div>
-
 		</div>
-	</div>
 	</v-flex>
 </template>
 <script>
-export default {
-  name: 'Settingmdodal',
-  props: ['settings'],
-  data: function (){
-  	return {
-  		countries: {'AU':	'Australia', 'BR':	'Brazil','CA':	'Canada', 'CN':	'China','TW':	'Taiwan','GB':	'United Kingdom','US':	'United States'},
-  		btnColor: ['red', 'orange', 'yellow', 'green','blue', 'pink', 'purple'],
-  	}
-  }
- }
+	export default {
+	name: 'Settingmdodal',
+	props: ['settings'],
+	data: function (){
+		return {
+			countries: {'AU':	'Australia', 'BR':	'Brazil','CA':	'Canada', 'CN':	'China','TW':	'Taiwan','GB':	'United Kingdom','US':	'United States'},
+			btnColor: ['red', 'orange', 'yellow', 'green','blue', 'pink', 'purple'],
+		}
+	}
+}
 </script>
 <style scoped>
 	.header{
@@ -56,11 +61,11 @@ export default {
 	.setting-select {
 		width: 45%;
 		border: 1px solid #aaa;
-	    box-shadow: 0 1px 0 1px rgba(0,0,0,.04);
-	    border-radius: .5em;
-	    -moz-appearance: none;
-	    -webkit-appearance: none;
-	    appearance: none;
+		box-shadow: 0 1px 0 1px rgba(0,0,0,.04);
+		border-radius: .5em;
+		-moz-appearance: none;
+		-webkit-appearance: none;
+		appearance: none;
 	}
 	.setting-row {
 		padding-top: 1em;
