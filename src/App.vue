b940650716acba5131e3981cbf67150a78e8f67d
James <james@brandboom.com>
2019-08-02 11:31:18 -0700

    UPDATE spacing and formatting fixes for App.vue and itunessearchapi.js, udated to all tabs

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
 <template>
-  <v-app>
-    <v-content>
-      <Search/>
-    </v-content>
-  </v-app>
+	<v-app>
+		<v-content>
+		<Search/>
+		</v-content>
+	</v-app>
 </template>
 
 <script>
-  import Search from './components/Search'
+import Search from './components/Search'
 
-  export default {
-    components: {
-      Search
-    }
-  }
+export default {
+	components: {
+		Search
+	}
+}
 </script>
 
 <style>
-  .application {
-    background-image: linear-gradient(320deg, #000000 0%, #554e59 75%) !important;
-  }
-  .v--modal-overlay .v--modal-box {
-    width: 30em;
-    height: 60em;
-    background: linear-gradient(to right, #e7d8fa 0, #b6b3d1 50%, #d8b2b2 100%);
-    border-radius: 30px;
-    font-family: Lato;
-  }
+.application {
+	background-image: linear-gradient(320deg, #000000 0%, #554e59 75%) !important;
+}
+.v--modal-overlay .v--modal-box {
+	width: 30em;
+	height: 60em;
+	background: linear-gradient(to right, #e7d8fa 0, #b6b3d1 50%, #d8b2b2 100%);
+	border-radius: 30px;
+	font-family: Lato;
+}
 </style>
 
