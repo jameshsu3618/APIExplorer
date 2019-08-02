90d3a5d1d971e6d246ad680d22b8477c9ef38a77
James <james@brandboom.com>
2019-08-02 11:10:23 -0700

    UPDATED deploy and vue files updated for itunesexplorer

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:jameshsu3618/jameshsu3618itunes.git master:gh-pages
+git push -f git@github.com:jameshsu3618/itunesexplorer.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	publicPath : '/jameshsu3618itunes/'
+	publicPath : '/itunesexplorer/'
 }
